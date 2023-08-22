6834d7bc50480ae903aede0fd0f61654c8b60362
Femi Sunmola <femcast@yahoo.com>
2023-08-21 19:44:25 -0600

    feat: added ec2-with-datablock

---
+++ ec2-with-datablock/data.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "redhat_9" {
+    most_recent      = true
+    owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9.0.0_HVM-*-x86_64-24-Hourly2-GP2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
---
+++ ec2-with-datablock/ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "launch_page" {
+  ami = data.aws_ami.redhat_9.id
+  instance_type = var.instance_type
+  user_data = file("${path.module}/user-data.sh")
+  key_name = var.key_pair
+  vpc_security_group_ids = [aws_security_group.allow_tls.id, aws_security_group.allow_ssh.id]
+  tags = {
+    "Name" = "Launcher"
+  }
+}
+
