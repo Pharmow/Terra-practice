5199b00fe3b8f8754300b688bfcba0444ae5322b
Femi Sunmola <femcast@yahoo.com>
2023-08-21 19:22:56 -0600

    2nd commit

--- semver.sh
+++ semver.sh
@@ -1,23 +1,23 @@
-npm install semantic-release @semantic-release/git @semantic-release/github -D
+# npm install semantic-release @semantic-release/git @semantic-release/github -D
 
-cat <<EOF > release.config.js
-module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/Pharmow/Terra-practice.git",
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/git',
-        '@semantic-release/github'
-    ]
-}
-EOF
-# Enter the url of the github repo you want to SemVer on line 6 
+# cat <<EOF > release.config.js
+# module.exports = {
+#     branches: "main",
+#     repositoryUrl: "https://github.com/Pharmow/Terra-practice.git",
+#     plugins: [
+#         '@semantic-release/commit-analyzer',
+#         '@semantic-release/release-notes-generator',
+#         '@semantic-release/git',
+#         '@semantic-release/github'
+#     ]
+# }
+# EOF
+# # Enter the url of the github repo you want to SemVer on line 6 
 
-# Semantic versioning nomenclature
-# fix for minor change (patching)
-# feat for slight change (minor version)
-# BREAKING CHANGE for major change (major version)
+# # Semantic versioning nomenclature
+# # fix for minor change (patching)
+# # feat for slight change (minor version)
+# # BREAKING CHANGE for major change (major version)
 
 # mkdir -pv .github/workflows
 # cd ./.github/workflows
